3236644ccb56c1cdc9066ca63610a8d60abd6364
jair david <cjair759@gmail.com>
2026-03-18 12:24:08 -0500

    se sube actualizaciond erutas para blog 2

---
+++ src/models/blog_media.model.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const BlogMedia = sequelize.define('blog_media', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'blogs',
+        key: 'id'
+      }
+    },
+    media_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'media',
+        key: 'id'
+      }
+    },
+    position: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    caption: {
+      type: DataTypes.STRING(512),
+      allowNull: true
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at',
+      defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: 'updated_at'
+    }
+  }, {
+    tableName: 'blog_media',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  });
+
+  return BlogMedia;
+};
